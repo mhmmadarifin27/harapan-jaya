a74bbab7c82c5d90a3fe705bf1f0e9748e610d48
Muhammad Arifin <Arifinkembar2@gmail.com>
2026-03-21 00:26:15 +0700

    src/app/sitemap.js

---
+++ src/app/sitemap.js
@@ -0,0 +1,11 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://harapanjaya.web.id',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    // Tambahkan halaman lain jika ada, misal /menu
+  ]
+}
