18eb25ba48495a4214842f405ced8ebdcdc087c0
Muhammad Arifin <Arifinkembar2@gmail.com>
2026-03-27 15:09:25 +0700

    src/app/page.jspublic/rendang2.jpeg

--- src/app/page.js
+++ src/app/page.js
@@ -36,7 +36,7 @@ export default function Home() {
 
   // --- DATA MENU & ULASAN ---
   const menuItems = [
-    { id: 1, tag: "THE ICON", name: "Rendang Sapi", desc: "Dimasak perlahan selama 8 jam dalam kuali dengan 12 bumbu rahasia.", price: "Rp 17.000", image: "/rendang.jpeg" },
+    { id: 1, tag: "THE ICON", name: "Rendang Sapi", desc: "Dimasak perlahan selama 8 jam dalam kuali dengan 12 bumbu rahasia.", price: "Rp 17.000", image: "/rendang2.jpeg" },
     { id: 2, tag: "CHEF'S SPECIAL", name: "Gulai Kepala Baung", desc: "Olahan kepala ikan Baung segar dengan kuah kuning kental yang kaya rempah dan aroma yang memikat.", price: "Rp 30.000", image: "/makanan-hero-2.jpeg" },
     { id: 3, tag: "CLASSIC FAVORITE", name: "Ayam Goreng Kremes", desc: "Ayam ungkep bumbu kuning yang digoreng garing dengan taburan kremesan gurih nan renyah.", price: "Rp 17.000", image: "/ayam.jpeg" }
   ];
